05bafcc4497953373584d272f5b68a22f0b52e1e
vnhaxnet <66384697+vnhaxnet@users.noreply.github.com>
2020-07-15 11:34:52 +0700

    Create ld2qt.bat

---
+++ ld2qt.bat
@@ -0,0 +1,8 @@
+@echo off
+@cd/d "C:\XuanZhi\LDPlayer"
+@cd/d "D:\XuanZhi\LDPlayer"
+@cd/d "E:\XuanZhi\LDPlayer"
+@cd/d "F:\XuanZhi\LDPlayer"
+@cd/d "Y:\XuanZhi\LDPlayer"
+@cd/d "H:\XuanZhi\LDPlayer"
+dnconsole.exe launchex --index 1 --packagename "com.tencent.ig"
