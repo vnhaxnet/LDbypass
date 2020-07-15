35f9b5b3db443c7f2346dd476db982b50f912067
vnhaxnet <66384697+vnhaxnet@users.noreply.github.com>
2020-07-15 11:40:45 +0700

    Create conqt.bat

---
+++ conqt.bat
@@ -0,0 +1,7 @@
+@echo off
+@cd/d "C:\XuanZhi\LDPlayer"
+@cd/d "D:\XuanZhi\LDPlayer"
+@cd/d "E:\XuanZhi\LDPlayer"
+@cd/d "F:\XuanZhi\LDPlayer"
+@cd/d "Y:\XuanZhi\LDPlayer"
+dnconsole.exe launchex --index 0 --packagename "com.tencent.ig"
