d952d9e9200d082a2db364a6aa4410730c99de79
vnhaxnet <66384697+vnhaxnet@users.noreply.github.com>
2020-08-02 11:56:15 +0700

    Update runld1.bat

--- runld1.bat
+++ runld1.bat
@@ -1,8 +1,1 @@
-@echo off
-@cd/d "C:\XuanZhi\LDPlayer"
-@cd/d "D:\XuanZhi\LDPlayer"
-@cd/d "E:\XuanZhi\LDPlayer"
-@cd/d "F:\XuanZhi\LDPlayer"
-@cd/d "Y:\XuanZhi\LDPlayer"
-@cd/d "H:\XuanZhi\LDPlayer"
 dnconsole launch --index 1
